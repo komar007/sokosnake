003a5f40a17540f04446557ac22dc166cfe6900f
Michal Trybus <komar007@gmail.com>
2009-07-30 22:01:26 +0200

    [sokosnake] Begun work in new qt interface, renamed the old interface to qt_interface_simple

---
+++ qt_interface/qt_interface.py
@@ -0,0 +1,20 @@
+import sys
+from PyQt4 import QtCore, QtGui, QtSvg
+from sokosnakewindow import Ui_SokosnakeWindow
+
+class SokosnakeWindow(QtGui.QMainWindow):
+	def __init__(self, parent=None):
+		QtGui.QWidget.__init__(self, parent)
+		self.ui = Ui_SokosnakeWindow()
+		self.ui.setupUi(self)
+		QtCore.QObject.connect(self.ui.action_Open, QtCore.SIGNAL("activated()"), self.load_level)
+	
+	def load_level(self):
+		self.filename = QtGui.QFileDialog.getOpenFileName(self, "Choose a level...", "levels", "All Files (*.*)")
+
+if __name__ == "__main__":
+	app = QtGui.QApplication(sys.argv)
+	win = SokosnakeWindow()
+	win.show()
+	sys.exit(app.exec_())
+
---
+++ qt_interface/sokosnakeview.py
@@ -0,0 +1,4 @@
+from PyQt4 import QtGui
+
+class SokosnakeView(QtGui.QGraphicsView):
+	pass
